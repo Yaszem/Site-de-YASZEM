21c5dc569f112e33de3c17fd3c4bca107b8028b5
Yaszem <135852002+Yaszem@users.noreply.github.com>
2023-09-17 17:22:11 +0200

    Add files via upload

---
+++ SUITE/script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Vous pouvez ajouter ici le code pour envoyer le formulaire de contact, par exemple, via une API ou un service de messagerie.
+    alert("Votre message a été envoyé avec succès !");
+});
